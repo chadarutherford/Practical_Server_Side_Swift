f6447caf9204eab3a2cc6a316d1b06de6d6446e8
Chad Rutherford <chad.rutherford@pngaming.com>
2022-04-03 21:53:56 -0400

    Finished Select

---
+++ Sources/App/Framework/Form/Fields/SelectFieldTemplate.swift
@@ -0,0 +1,36 @@
+//
+//  SelectFieldTemplate.swift
+//  
+//
+//  Created by Rutherford, Chad on 4/3/22.
+//
+
+import SwiftHtml
+import Vapor
+
+public struct SelectFieldTemplate: TemplateRepresentable {
+    public var context: SelectFieldContext
+    
+    public init(_ context: SelectFieldContext) {
+        self.context = context
+    }
+    
+    @TagBuilder
+    public func render(_ req: Request) -> Tag {
+        LabelTemplate(context.label).render(req)
+        
+        Select {
+            for item in context.options {
+                Option(item.label)
+                    .value(item.key)
+                    .selected(context.value == item.key)
+            }
+        }
+        .name(context.key)
+        
+        if let error = context.error {
+            Span(error)
+                .class("error")
+        }
+    }
+}
---
+++ Sources/App/Framework/Form/Templates/Contexts/OptionContext.swift
@@ -0,0 +1,32 @@
+//
+//  File.swift
+//  
+//
+//  Created by Rutherford, Chad on 4/3/22.
+//
+
+import Foundation
+
+public struct OptionContext {
+    public var key: String
+    public var label: String
+    
+    public init(key: String, label: String) {
+        self.key = key
+        self.label = label
+    }
+}
+
+public extension OptionContext {
+    static func yesNo() -> [OptionContext] {
+        ["yes", "no"].map { .init(key: $0, label: $0.capitalized) }
+    }
+    
+    static func trueFalse() -> [OptionContext] {
+        [true, false].map { .init(key: String($0), label: String($0).capitalized) }
+    }
+    
+    static func numbers(_ numbers: [Int]) -> [OptionContext] {
+        numbers.map { .init(key: String($0), label: String($0)) }
+    }
+}
